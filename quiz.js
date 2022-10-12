f4821d5ad5dfd2622d8ef05f3385ad206c7bea47
kavinjayanth <sit20cs127@sairamtap.edu.in>
2022-10-12 23:15:30 +0530

    add new

--- quiz.js
+++ quiz.js
@@ -1,35 +1,110 @@
 //Question bank
 var questionBank= [
     {
-        question : 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
-        option : ['Asia','Africa','Europe','Australia'],
-        answer : 'Africa'
+        question : 'Whether the Aicte idea lab is present in sairam ?',
+        option : ['Yes','No'],
+        answer : 'Yes'
     },
     {
-        question : 'Garampani sanctuary is located at',
-        option : ['Junagarh, Gujarat','Diphu, Assam','Kohima, Nagaland','Gangtok, Sikkim'],
-        answer : 'Diphu, Assam'
+        question : 'The purpose of the SDGs is to',
+        option : ['Create 17 goals','Help all humans live sustainably','Engage millions of people and thousands of organisations','help'],
+        answer : 'Help'
     },
     {
-        question : 'For which of the following disciplines is Nobel Prize awarded?',
-        option : ['Physics and Chemistry','Physiology or Medicine','Literature, Peace and Economics','All of the above'],
-        answer : 'All of the above'
+        question : 'The SDGs are focussed on',
+        option : ['the past','the past and the present','the present','the present and the future'],
+        answer : 'the present and the future'
     },
     {
-        question : 'Hitler party which came into power in 1933 is known as',
-        option : ['Labour Party','Nazi Party','Ku-Klux-Klan','Democratic Party'],
-        answer : 'Nazi Party'
+        question : 'Which SDG aims to Conserve and sustainably use the oceans, seas and marine resources for sustainable development',
+        option : ['No Poverty','Good Health and Well-Being','Affordable and Clean Energy','Life below Water'],
+        answer : 'Life below Water'
     },
     {
-        question : 'First human heart transplant operation conducted by Dr. Christiaan Barnard on Louis Washkansky, was conducted in',
-        option : ['1967','1968','1958','1922'],
-        answer : '1967'
+        question : 'Which is not a purpose of the SDG ?',
+        option : ['to promote peace','to promote prosperity','to promote equality','to promote challenge'],
+        answer : 'to promote challenge'
     },
     {
-        question : 'when did india get independence ?',
-        option : ['1967','1968','1958','1947'],
-        answer : '1947'
-    }
+        question : 'The SDGs are',
+        option : ['independent of each other','linked to each other'],
+        answer : 'linked to each other'
+    },
+    {
+        question : 'Which is not an obstacle to achieving the SDG?',
+        option : ['differences in income','ecological problems','capital cities','using more than we produce or grow'],
+        answer : 'capital cities'
+
+    },
+    {
+        question : 'Who should be involved for the SDG to be met?',
+        option : ['Individuals','Governments','voluntary organisartions','private companies'],
+        answer : 'Governments'
+    },
+    {
+        question : 'Augmented reality, Virtual reality and Mixed reality can be put together as',
+        option : ['Artificial reality','Extended reality','Ultimate reality'],
+        answer : 'Extended reality'
+    },
+    {
+        question : 'Microsoft hololens 2 is a gear for',
+        option : ['Mixed reality','Virtual reality','Augmented reality'],
+        answer : 'Mixed reality'
+
+    },
+    {
+        question : 'Which of the following is a standalone VR gear',
+        option : ['Meta Quest 2','HTC Vive Pro 2','Oculus Rift'],
+        answer : 'Meta Quest 2'
+    },
+    {
+        question : 'How many 2d images are required to generate a 3d image ?',
+        option : ['2','3','360'],
+        answer : '2'
+    },
+    {
+        question : 'Which SOC is used in Meta Quest 2',
+        option : ['Nvidia RTX 3090Ti','Intel i7 12700k','Snapdragon XR2'],
+        answer : 'Snapdragon XR2'
+
+    },
+    {
+        question : 'What is full form of UAV?',
+        option : ['Unarmed Aircraft Vehicle','Unarmed Aerial Vehicle','Unmanned Aeroplance Vehicle','Unarmed Aircraft Vehicle'],
+        answer : 'Unarmed Aerial Vehicle'
+    },
+    {
+        question : 'Components of Unmanned Aircraft System (UAS) does not consists of ____',
+        option : ['UAV','Ground based controller','Air strip for landing','A communication system between UAV and Controller'],
+        answer : 'Air strip for landing'
+    },
+    {
+        question : 'Which of the following is not a term used for drones?',
+        option : ['Rapid Action Fight System (RAFS)','Unmanned Aircraft Vehicle System (UAVS)','Remotely Piloted Aerial System (RPAS)','Remotely Piloted Aircraft System (RPAS)'],
+        answer : 'Rapid Action Fight System (RAFS)'
+    },
+    {
+        question : 'Which of the following category of drones does not require registration for use in India?',
+        option : ['Micro Category','Small Category','Nano Category','Large Category'],
+        answer : 'Nano Category'
+    },
+    {
+        question : 'As per drone categories in India weight of the Nano drone should be ____',
+        option : ['More than 250gram','Less or equal to 250gram','250gram to 2kg','2kg to 4kg'],
+        answer : 'Less or equal to 250gram'
+    },
+    {
+        question : 'permit is not required for Nano category drones to fly upto ____ feet',
+        option : ['150','50','200','100'],
+        answer : '50'
+    },
+    {
+        question : 'A permit is not required for Micro category drones to fly upto ____ feet',
+        option : ['100','300','250','200'],
+        answer : '200'
+    },
+
+
 ]
 
 var question= document.getElementById('question');
